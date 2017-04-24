e5eb88b926bdef83f0a53b64497db87345664c37
hongzhiyuan <hongzhiyuan@baidu.com>
2017-04-24 13:41:41 +0800

    update local

--- resources/views/submitTask.php
+++ resources/views/submitTask.php
@@ -18,8 +18,23 @@
     <![endif]-->
 </head>
 <body>
-<h1>你好，世界！</h1>
+<div class="container">
 
+    <form class="form-signin">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+            <label>
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    </form>
+
+</div> <!-- /container -->
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
