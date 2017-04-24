ad73ea21c5e6999ca6b5bb1a330ea30278009ade
hongzhiyuan <hongzhiyuan@baidu.com>
2017-04-25 03:10:01 +0800

    update template

--- resources/views/course/item.blade.php
+++ resources/views/course/item.blade.php
@@ -1,26 +1,35 @@
 @extends('layouts.base')
-
 @section('content')
-    <div class="banner">
-        <div class="banner-title">一起蹭課吧</div>
-    </div>
     <article class="weui-article">
+        <h1>{{$course->title}}</h1>
         <section>
-            <h2 class="title">我们是谁</h2>
+            <h2 class="title">课程介绍</h2>
             <section>
-                <p>这里是描述</p>
+                <h3>讲师：{{$course->teacher_name}}</h3>
+                <p>
+                    {{$course->teacher_desc}}
+                </p>
             </section>
-        </section>
-        <section>
-            <h2 class="title">我们的优势</h2>
             <section>
-                <p>这里是我们的优势</p>
+                <h3>课程内容：</h3>
+                <p>
+                    {{$course->description}}
+                </p>
+            </section>
+            <section>
+                <p>
+                    时间：{{$course->start_time}}
+                </p>
             </section>
         </section>
     </article>
-    <div>
-    </div>
-    <div class="footer-btns">
-        <a href="/teacher/submit" class="weui-btn weui-btn_primary">我要讲课</a>
+    <label for="weuiAgree" class="weui-agree">
+        <input id="weuiAgree" type="checkbox" class="weui-agree__checkbox">
+            <span class="weui-agree__text">
+                阅读并同意<a href="javascript:void(0);">《用户协议》</a>
+            </span>
+    </label>
+    <div class="weui-btn-area">
+        <a class="weui-btn weui-btn_primary" href="javascript:" id="showTooltips">我要听课（￥9.9）</a>
     </div>
 @endsection
